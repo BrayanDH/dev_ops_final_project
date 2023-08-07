a1ca716c5d95520de31e792202e73d39429290bd
Brayan H <bdhdavid.bh@gmail.com>
2023-08-06 23:45:30 -0600

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -11,11 +11,13 @@ from service.common import log_handlers
 from flask_talisman import Talisman
 from flask_cors import CORS
 
+
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
 talisman = Talisman(app)
 CORS(app)
+
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
@@ -39,6 +41,7 @@ except Exception as error:  # pylint: disable=broad-except
     sys.exit(4)
 
 app.logger.info("Service initialized!")
+
 
 @app.after_request
 def add_security_headers(response):
